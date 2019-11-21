f9b37724e782130bc0bd9cfd67850a03fb43d5c7
git <lixiaokang215@163.com>
2019-11-22 07:16:21 +0800

    Initial commit

---
+++ src/main/java/com/smartmining/install/es/ComSmartminingInstallEsApplication.java
@@ -0,0 +1,13 @@
+package com.smartmining.install.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComSmartminingInstallEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComSmartminingInstallEsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartmining/install/es/ComSmartminingInstallEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmining.install.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComSmartminingInstallEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
