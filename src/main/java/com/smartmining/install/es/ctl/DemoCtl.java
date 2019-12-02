b880fc65b28a901b1d5dd39cae657d8b2af428e2
lxk <lixiaokang215@163.com>
2019-12-02 12:50:18 +0800

    按资源id导入

--- src/main/java/com/smartmining/install/es/ctl/DemoCtl.java
+++ src/main/java/com/smartmining/install/es/ctl/DemoCtl.java
@@ -8,10 +8,7 @@ import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.JestResult;
 import io.searchbox.client.config.HttpClientConfig;
-import io.searchbox.core.Bulk;
-import io.searchbox.core.BulkResult;
-import io.searchbox.core.Index;
-import io.searchbox.core.Search;
+import io.searchbox.core.*;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
@@ -38,66 +35,66 @@ public class DemoCtl {
     private Environment env;
 
 
-    @RequestMapping("/exportz")
-    public String exportz(@RequestParam String code, HttpServletResponse response) {
-        JestClient jestClient = null;
-        try {
-            String indexName = env.getProperty("es.index.name");
-            String typeName = env.getProperty("es.index.type");
-            String elasticIps = env.getProperty("es.address");
-            int maxsize = Integer.parseInt(env.getProperty("es.maxrow"));
-//            String indexName = "x_restree_dev";
-//            String typeName = "def_dev";
-//            String elasticIps = "http://192.168.0.23:9200";
-            JestClientFactory factory = new JestClientFactory();
-            factory.setHttpClientConfig(new HttpClientConfig.Builder(elasticIps).connTimeout(60000).readTimeout(60000).multiThreaded(true).build());
-            jestClient = factory.getObject();
-            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-            searchSourceBuilder.from(0).size(maxsize);
-            searchSourceBuilder.query(QueryBuilders.termQuery("c_tenant_one_code", code));
-            Search search = new Search.Builder(searchSourceBuilder.toString())
-                    .addIndex(indexName)
-                    .addType(typeName)
-                    .build();
-            JestResult jr = jestClient.execute(search);
-            List<String> results = jr.getSourceAsStringList();
-            if (results.size() < 0) {
-                logger.warn("没有查询到数据");
-                return "没有查询到数据";
-            }
-            JSONArray dataArr = new JSONArray();
-            for (int i = 0; i < results.size(); i++) {
-                dataArr.add(JSON.parseObject(results.get(i)));
-            }
-            byte[] bytes = JSON.toJSONBytes(dataArr);
-            try {
-                String fileName = code + "_" + getToDayDateStr() + ".json";
-                response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
-                response.addHeader("filename", fileName);
-                OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
-                response.setContentType("*/*");
-                toClient.write(bytes);
-                toClient.flush();
-                toClient.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-                logger.error("文件下载异常！", e);
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (jestClient != null) {
-                try {
-                    jestClient.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return "success";
-    }
+//    @RequestMapping("/exportz")
+//    public String exportz(@RequestParam String code, HttpServletResponse response) {
+//        JestClient jestClient = null;
+//        try {
+//            String indexName = env.getProperty("es.index.name");
+//            String typeName = env.getProperty("es.index.type");
+//            String elasticIps = env.getProperty("es.address");
+//            int maxsize = Integer.parseInt(env.getProperty("es.maxrow"));
+////            String indexName = "x_restree_dev";
+////            String typeName = "def_dev";
+////            String elasticIps = "http://192.168.0.23:9200";
+//            JestClientFactory factory = new JestClientFactory();
+//            factory.setHttpClientConfig(new HttpClientConfig.Builder(elasticIps).connTimeout(60000).readTimeout(60000).multiThreaded(true).build());
+//            jestClient = factory.getObject();
+//            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+//            searchSourceBuilder.from(0).size(maxsize);
+//            searchSourceBuilder.query(QueryBuilders.termQuery("c_tenant_one_code", code));
+//            Search search = new Search.Builder(searchSourceBuilder.toString())
+//                    .addIndex(indexName)
+//                    .addType(typeName)
+//                    .build();
+//            JestResult jr = jestClient.execute(search);
+//            List<String> results = jr.getSourceAsStringList();
+//            if (results.size() < 0) {
+//                logger.warn("没有查询到数据");
+//                return "没有查询到数据";
+//            }
+//            JSONArray dataArr = new JSONArray();
+//            for (int i = 0; i < results.size(); i++) {
+//                dataArr.add(JSON.parseObject(results.get(i)));
+//            }
+//            byte[] bytes = JSON.toJSONBytes(dataArr);
+//            try {
+//                String fileName = code + "_" + getToDayDateStr() + ".json";
+//                response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
+//                response.addHeader("filename", fileName);
+//                OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+//                response.setContentType("*/*");
+//                toClient.write(bytes);
+//                toClient.flush();
+//                toClient.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                logger.error("文件下载异常！", e);
+//
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (jestClient != null) {
+//                try {
+//                    jestClient.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//        return "success";
+//    }
 
     /**
      * 如果上传的地方upload.html中的名称如果和这里的参数MultipartFile名称一致的话就不用加@RequestParm注解，
@@ -114,53 +111,117 @@ public class DemoCtl {
         //保存文件
 //        multipartFile.transferTo(new File("e:/"+multipartFile.getOriginalFilename()));
 
-        String indexName = env.getProperty("es.index.name");
-        String typeName = env.getProperty("es.index.type");
+        String envName = env.getProperty("es.index.env");
         String elasticIps = env.getProperty("es.address");
+        String treeIndexName = "x_restree" + envName;
+        String bloodIndexName = "x_blood" + envName;
+        String typeName = "def" + envName;
+
         JestClientFactory factory = new JestClientFactory();
         factory.setHttpClientConfig(new HttpClientConfig.Builder(elasticIps).connTimeout(60000).readTimeout(60000).multiThreaded(true).build());
         JestClient jestClient = factory.getObject();
-        List<JSONObject> resData = null;
+        JSONObject importData;
         // JSON 文件 解析成 List<XOpsAudit>对象
         try {
             String jsonStr = JSON.toJSONString(JSON.parse(multipartFile.getBytes()));
-            resData = JSON.parseObject(jsonStr, new TypeReference<List<JSONObject>>() {
+            importData = JSON.parseObject(jsonStr, new TypeReference<JSONObject>() {
             });
         } catch (IOException e) {
             e.printStackTrace();
             logger.error("文件解析异常！", e);
 
+            return "文件解析异常";
         }
-        if (resData == null || resData.size() < 1) {
+        List<JSONObject> treeData = importData.getJSONArray("treedata").toJavaList(JSONObject.class);
+        List<JSONObject> bloodData = importData.getJSONArray("blooddata").toJavaList(JSONObject.class);
+        if (treeData == null || treeData.size() < 1) {
             logger.error("无对应数据！");
             return "无对应数据！";
         }
-
         // id设置为null，直接新增
-        resData.stream().forEach(e -> {
+        treeData.stream().forEach(e -> {
             e.put("c_tenant_one_code", code);
+            e.put("c_id", e.getString("c_id").replace(e.getString("c_tenant_one_code"), code));
 //            e.put("c_id",UUID.randomUUID().toString().replace("-",""));//测试用
             logger.debug(e.toJSONString());
         });
 
-        Bulk.Builder bulk = new Bulk.Builder().defaultIndex(indexName).defaultType(typeName);
+        Bulk.Builder bulk = new Bulk.Builder().defaultIndex(treeIndexName).defaultType(typeName);
         Index index;
         BulkResult br;
         int cnt = 0;
 
-        for (JSONObject obj : resData) {
+        for (JSONObject obj : treeData) {
             cnt++;
             index = new Index.Builder(obj).id(obj.getString("c_id")).build();
+            bulk.addAction(index);
+            batchDeleteBlood(obj.getString("c_id"), bloodIndexName, typeName, jestClient);
+            if (cnt % 500 == 0) {
+                br = jestClient.execute(bulk.build());
+                logger.debug(br.isSucceeded() + "");
+            }
+        }
+
+        br = jestClient.execute(bulk.build());
+        logger.debug(br.isSucceeded() + "");
+
+
+        batchInsertBlood(bloodData,bloodIndexName,typeName,jestClient);
+
+        jestClient.close();
+        return "success,total=" + treeData.size();
+    }
+
+    private void batchDeleteBlood(String id, String bloodIndexName, String typeName, JestClient jestClient) throws IOException {
+        Bulk.Builder bulk = new Bulk.Builder().defaultIndex(bloodIndexName).defaultType(typeName);
+        SearchSourceBuilder searchSourceBuilder2 = new SearchSourceBuilder();
+        searchSourceBuilder2.from(0).size(20000);
+        searchSourceBuilder2.query(QueryBuilders.termQuery("c_id", id));
+        Search search2 = new Search.Builder(searchSourceBuilder2.toString())
+                .addIndex(bloodIndexName)
+                .addType(typeName)
+                .build();
+        List<SearchResult.Hit<JSONObject, Void>> searchResults =
+                jestClient.execute(search2)
+                        .getHits(JSONObject.class);
+        searchResults.forEach(hit -> {
+            logger.debug(String.format("Document %s has score %s", hit.id, hit.score));
+            Delete dr = new Delete.Builder(hit.id).build();
+            bulk.addAction(dr);
+
+        });
+//        BulkResult   br = jestClient.execute(bulk.build());
+//        logger.debug(br.isSucceeded() + "");
+
+
+//        List<String> results2 = jr2.getSourceAsStringList();
+//        if (results2.size() < 1) {
+//            logger.warn("没有查询到数据");
+//            return;
+//        }
+//        for (int i = 0; i < results2.size(); i++) {
+//            dr= new Delete.Builder(results2.get(i).).build();
+//            bulk.addAction(dr);
+//        }
+
+    }
+
+    private void batchInsertBlood(List<JSONObject> bloodData, String bloodIndexName, String typeName, JestClient jestClient) throws IOException {
+        Bulk.Builder bulk = new Bulk.Builder().defaultIndex(bloodIndexName).defaultType(typeName);
+        Index index;
+        BulkResult br;
+        int cnt = 0;
+        for (JSONObject obj : bloodData) {
+            cnt++;
+            index = new Index.Builder(obj).build();
             bulk.addAction(index);
             if (cnt % 500 == 0) {
                 br = jestClient.execute(bulk.build());
                 logger.debug(br.isSucceeded() + "");
             }
         }
-        br = jestClient.execute(bulk.build());
-        logger.debug(br.isSucceeded() + "");
-        jestClient.close();
-        return "success,total=" + resData.size();
+//        br = jestClient.execute(bulk.build());
+//        logger.debug(br.isSucceeded() + "");
     }
 
     private String getToDayDateStr() {
@@ -172,9 +233,11 @@ public class DemoCtl {
     public String exportsub(@RequestParam String id, HttpServletResponse response) {
         JestClient jestClient = null;
         try {
-            String indexName = env.getProperty("es.index.name");
-            String typeName = env.getProperty("es.index.type");
+            String envName = env.getProperty("es.index.env");
             String elasticIps = env.getProperty("es.address");
+            String treeIndexName = "x_restree" + envName;
+            String bloodIndexName = "x_blood" + envName;
+            String typeName = "def" + envName;
             int maxsize = Integer.parseInt(env.getProperty("es.maxrow"));
 //            String indexName = "x_restree_dev";
 //            String typeName = "def_dev";
@@ -186,7 +249,7 @@ public class DemoCtl {
             searchSourceBuilder.from(0).size(maxsize);
             searchSourceBuilder.query(QueryBuilders.termQuery("c_id", id));
             Search search = new Search.Builder(searchSourceBuilder.toString())
-                    .addIndex(indexName)
+                    .addIndex(treeIndexName)
                     .addType(typeName)
                     .build();
             JestResult jr = jestClient.execute(search);
@@ -196,10 +259,35 @@ public class DemoCtl {
                 return "没有查询到数据";
             }
             JSONArray dataArr = new JSONArray();
+
             JSONObject jsonObject = JSON.parseObject(results.get(0));
             dataArr.add(jsonObject);
-            recursionByPid(jsonObject.getString("c_id"), dataArr, jestClient, indexName, typeName, maxsize);
-            byte[] bytes = JSON.toJSONBytes(dataArr);
+
+            JSONArray bloodArr = new JSONArray();
+
+            SearchSourceBuilder searchSourceBuilder2 = new SearchSourceBuilder();
+            searchSourceBuilder2.from(0).size(maxsize);
+            searchSourceBuilder2.query(QueryBuilders.termQuery("c_id", id));
+            Search search2 = new Search.Builder(searchSourceBuilder2.toString())
+                    .addIndex(bloodIndexName)
+                    .addType(typeName)
+                    .build();
+            JestResult jr2 = jestClient.execute(search2);
+            List<String> results2 = jr2.getSourceAsStringList();
+            if (results2.size() < 1) {
+                logger.warn("没有查询到数据");
+                return "没有查询到数据";
+            }
+            for (int i = 0; i < results2.size(); i++) {
+                bloodArr.add(JSON.parseObject(results2.get(i)));
+            }
+
+
+            recursionByPid(jsonObject.getString("c_id"), dataArr, bloodArr, jestClient, treeIndexName, bloodIndexName, typeName, maxsize);
+            JSONObject jsonResult = new JSONObject();
+            jsonResult.put("treedata", dataArr);
+            jsonResult.put("blooddata", bloodArr);
+            byte[] bytes = JSON.toJSONBytes(jsonResult);
             try {
                 String fileName = id + "_" + getToDayDateStr() + ".json";
                 response.addHeader("Content-Disposition", "attachment;filename=" + fileName);
@@ -230,12 +318,12 @@ public class DemoCtl {
     }
 
 
-    private void recursionByPid(String pid, JSONArray dataArr, JestClient jestClient, String indexName, String typeName, int maxsize) throws IOException {
+    private void recursionByPid(String pid, JSONArray dataArr, JSONArray bloodArr, JestClient jestClient, String treeIndexName, String bloodIndexName, String typeName, int maxsize) throws IOException {
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         searchSourceBuilder.from(0).size(maxsize);
         searchSourceBuilder.query(QueryBuilders.termQuery("c_pid", pid));
         Search search = new Search.Builder(searchSourceBuilder.toString())
-                .addIndex(indexName)
+                .addIndex(treeIndexName)
                 .addType(typeName)
                 .build();
         JestResult jr = jestClient.execute(search);
@@ -245,10 +333,32 @@ public class DemoCtl {
             return;
         } else {
             JSONObject jsonObject;
+            SearchSourceBuilder searchSourceBuilder2;
+            Search search2;
+            JestResult jr2;
+            List<String> results2;
             for (int i = 0; i < results.size(); i++) {
                 jsonObject = JSON.parseObject(results.get(i));
                 dataArr.add(jsonObject);
-                recursionByPid(jsonObject.getString("c_id"), dataArr, jestClient, indexName, typeName, maxsize);
+
+                searchSourceBuilder2 = new SearchSourceBuilder();
+                searchSourceBuilder2.from(0).size(maxsize);
+                searchSourceBuilder2.query(QueryBuilders.termQuery("c_id", jsonObject.getString("c_id")));
+                search2 = new Search.Builder(searchSourceBuilder2.toString())
+                        .addIndex(bloodIndexName)
+                        .addType(typeName)
+                        .build();
+                jr2 = jestClient.execute(search2);
+                results2 = jr2.getSourceAsStringList();
+                if (results2.size() < 1) {
+                    logger.warn("没有查询到数据");
+                    continue;
+                }
+                for (int j = 0; j < results2.size(); j++) {
+                    bloodArr.add(JSON.parseObject(results2.get(j)));
+                }
+
+                recursionByPid(jsonObject.getString("c_id"), dataArr, bloodArr, jestClient, treeIndexName, bloodIndexName, typeName, maxsize);
             }
         }
     }
